832e47aee13ffec939642ce80829e5a01baacc2a
Akshit Batra <akbatra567@gmail.com>
2023-01-17 23:56:43 +0530

    [Akshit] Add. basic routes with message

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const port = process.env.port || 3000; 
+
+app.get('/addNewExpense', (req,res)=> {
+    res.send("Add new expense page");
+});
+
+app.get('/viewExpenseSummary', (req,res)=> {
+    res.send("Show expense summary page");
+});
+
+app.get('/viewAllExpenses', (req,res)=> {
+    res.send("View all expenses screen");
+});
+
+app.get('/viewMonthlyExpensesByCategory', (req,res)=> {
+    res.send("View monthly expenses by category");
+});
+
+app.listen(port, ()=> {
+    console.log(`Server started at ${port}`);
+})
